945762df2a7b064b0b25cfc9c2e5bdb92d8eb120
Anna-19 <akalgudaeva@yandex.ru>
2023-11-21 09:48:14 +0300

    1 laba

--- timp_1_lab/2/main.cpp
+++ timp_1_lab/2/main.cpp
@@ -1,13 +1,7 @@
 #include "modAlphaCipher.h"
 using namespace std;
 
-bool isValid(const wstring& s)
-{
-    for(auto c:s)
-        if (!iswalpha(c) || !iswupper(c))
-            return false;
-    return true;
-}
+
 int main(int argc, char **argv)
 {
     locale loc("ru_RU.UTF-8");
@@ -31,15 +25,13 @@ int main(int argc, char **argv)
         } else if (op >0) {
             wcout<<"Cipher ready. Input text: ";
             wcin>>text;
-            if (isValid(text)) {
-                if (op==1) {
-                    wcout<<"Encrypted text: "<<cipher.encrypt(text)<<endl;
-                } else {
-                    wcout<<"Decrypted text: "<<cipher.decrypt(text)<<endl;
-                }
-            } else {
-                wcout<<"Operation aborted: invalid text\n";
-            }
+            
+        	if (op==1) {
+            	wcout<<"Encrypted text: "<<cipher.encrypt(text)<<endl;
+        	} else {
+            	wcout<<"Decrypted text: "<<cipher.decrypt(text)<<endl;
+        	}
+    
         }
     } while (op!=0);
     return 0;
